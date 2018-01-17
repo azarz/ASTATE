8389dcc6a1787265c37d57050a44ed537d66e7df
arnaudgregoire <arnaud.gregoire@ensg.eu>
2018-01-17 17:08:59 +0100

    travail du 17/02/2018

--- sonde/serverAPI.js
+++ sonde/serverAPI.js
@@ -1,48 +1,140 @@
 'use strict'
 const express = require('express');
-const http = require('http');
-const cors = require('cors');
-const app = express();
+const Influx  = require('influx');
+const http    = require('http');
+const cors    = require('cors');
+const os      = require('os');
+const app     = express();
 
 app.use(cors());
 
-app.get('/test', function (req, res) {
+
+const influx = new Influx.InfluxDB({
+ host: 'localhost',
+ database: 'dbtest',
+ schema: 
+ [
+   {
+   	measurement: 'location',
+   	fields:{
+   		date      : Influx.FieldType.STRING,
+   		longitude : Influx.FieldType.FLOAT, 
+   		latitude  : Influx.FieldType.FLOAT
+   	},
+   	tags:[
+   		'host'
+   	]
+   },
+   {
+     measurement: 'measures',
+     fields: {
+       date           : Influx.FieldType.STRING,
+       temperature    : Influx.FieldType.FLOAT,
+       pressure       : Influx.FieldType.FLOAT,
+       humidity       : Influx.FieldType.FLOAT,
+       luminosity     : Influx.FieldType.FLOAT,
+       wind_heading   : Influx.FieldType.FLOAT,
+       wind_speed_avg : Influx.FieldType.FLOAT,
+	   wind_speed_max : Influx.FieldType.FLOAT,
+	   wind_speed_min : Influx.FieldType.FLOAT
+     },
+     tags: [
+       'host'
+     ]
+   },
+   {
+   	measurement: 'rainfall',
+   	fields:{
+   		date : Influx.FieldType.STRING
+   	},
+   	tags:[
+   		'host'
+   	]
+   }
+ ]
+})
+
+
+
+app.get(['/last/all','/last'], function (req, res) {
 
 	console.log("request in coming from " + req.connection.remoteAddress);
-	let json = `{
-		"location" : 
-			[
-				{
-					"date"      : "103231.19",
-					"longitude" : 47.613761,
-					"latitude"  : 1.368991
-				}
-			],
-		"rainfall" : 
-			[
-				{
-					"date"  : "2018-01-16T10:33:20.598Z",
-					"value" : 30
-				}
-			],
-		"measurements" : 
-			[
-				{
-					"date"          : "2018-01-16T10:30:52.580Z",
-				    "temperature"   : 10.00,
-				    "pressure"      : 995.03,
-				    "humidity"      : 49.9,
-			        "luminosity"    : 52852,
-			        "wind_heading"  : 173.396482453163,
-			        "wind_speed_avg": 38.1,
-				    "wind_speed_max": 68.5,
-				    "wind_speed_min": 24.1
-				}
-			]	
-	}`
-  	res.send(json);
 
-});
+	const promises = [];
 
-console.log("server listening on port 3000");
+	promises.push(
+		new Promise((resolve, reject) => {
+			  	influx.query(`
+				    select * from location
+				    where host = ${Influx.escape.stringLit(os.hostname())}
+				    order by time desc
+				    limit 1
+				  `).then(result => {
+				    //console.log('location')
+				    //console.log(result[0]);
+				    resolve(result[0]);
+
+				  }).catch(err => {
+				  	//console.log(err);
+				  	reject(err.stack);
+				})
+		})
+	)
+
+	promises.push(
+		new Promise((resolve, reject) => {
+		  	influx.query(`
+			    select * from measures
+			    where host = ${Influx.escape.stringLit(os.hostname())}
+			    order by time desc
+			    limit 1
+			  `).then(result => {
+			    resolve(result[0]);
+			  }).catch(err => {
+			  	reject(err.stack);
+			})
+		})
+	)
+
+
+	Promise.all(promises)
+	.then(promises => {
+		let location = promises[0];
+		let result   = promises[1];
+		let json = {
+			"location" : 
+				[
+					{
+						"date"      : location.date,
+						"longitude" : location.longitude,
+						"latitude"  : location.latitude
+					}
+				],
+			"rainfall" :
+				[
+					{
+						"date"  : "2018-01-16T10:33:20.598Z"
+					}
+				],
+			"measurements" : 
+				[
+					{
+						"date"          : result.date,
+					    "temperature"   : result.temperature,
+					    "pressure"      : result.pressure,
+					    "humidity"      : result.humidity,
+				        "luminosity"    : result.luminosity,
+				        "wind_heading"  : result.wind_heading,
+				        "wind_speed_avg": result.wind_speed_avg,
+					    "wind_speed_max": result.wind_speed_max,
+					    "wind_speed_min": result.wind_speed_min
+					}
+				]	
+		}
+		res.send(json);
+	})
+
+	//console.log("server listening on port 3000");
+})
+
 app.listen(3000);
--- sonde/sonde.js
+++ sonde/sonde.js
@@ -1,15 +1,28 @@
 'use strict'
-var fs = require('fs');
-const Influx = require('influx');
-
-const os = require('os');
-
+var fs           = require('fs');
+const Influx     = require('influx');
+const os         = require('os');
+const sensorsDir = '/var/log/sensors';
+const gpsDir     = '/var/log/gpsNmea';
+const rainDir    = '/var/log/rainCounter.log';
 
 
 const influx = new Influx.InfluxDB({
  host: 'localhost',
  database: 'dbtest',
- schema: [
+ schema: 
+ [
+   {
+   	measurement: 'location',
+   	fields:{
+   		date      : Influx.FieldType.STRING,
+   		longitude : Influx.FieldType.FLOAT, 
+   		latitude  : Influx.FieldType.FLOAT
+   	},
+   	tags:[
+   		'host'
+   	]
+   },
    {
      measurement: 'measures',
      fields: {
@@ -26,6 +39,15 @@ const influx = new Influx.InfluxDB({
      tags: [
        'host'
      ]
+   },
+   {
+   	measurement: 'rainfall',
+   	fields:{
+   		date : Influx.FieldType.STRING
+   	},
+   	tags:[
+   		'host'
+   	]
    }
  ]
 })
@@ -40,7 +62,9 @@ influx.getDatabaseNames()
   	}
   	if(!dbexist){
   		console.log("database does not exist, creation ...");
-  		return influx.createDatabase('dbtest');
+  		influx.createDatabase('dbtest');
+  		initSensors();
+  		initLocation();
   	}
   	console.log(names);
 	loop();
@@ -50,21 +74,227 @@ influx.getDatabaseNames()
     console.error(`Error creating Influx database!`);
   })
 
-function readSensors(file) {
 
-	fs.readFile(file, 'utf8', function(err, data) {  
-    if (err) throw err;
-  	let json = JSON.parse(data);
-  	 write(json);
-  });
+function initSensors() {
+	const p =	new Promise((resolve, reject) => {
+		let json;
+		fs.readFile(sensorsDir, 'utf8', function(err, data) {  
+		    if (err){
+		    	console.log(err);
+		    	reject(err);
+		    } 
+		  	json = JSON.parse(data);
+		  	//console.log('sensors');
+		  	//console.log(json);
+		  	resolve(json);
+		});
+	})
+	p.then(json =>writeSensors(json));
 }
 
-function extractSensors() {
-	
+
+function initLocation(argument) {
+	const p =	new Promise((resolve, reject) => {
+		let json;
+		fs.readFile(gpsDir, 'utf8', function(err, data) {  
+		    if (err){
+		    	console.log(err);
+		    	reject(err);
+		    } 
+		  	json = transformToJSON(data);
+		  	resolve(json);
+		});
+	})
+	p.then(json =>writeLocation(json));
 }
 
-function write(json) {
 
+function initRain(argument) {
+	const p =	new Promise((resolve, reject) => {
+		let json;
+		fs.readFile(rainDir, 'utf8', function(err, data) {  
+		    if (err){
+
+		    	reject(err);
+		    } 
+		  	json = transformLogToJSON(data);
+		  	resolve(json);
+		});
+	})
+	p.then(json =>writeRain(json));
+}
+
+function compareSensors() {
+	const promises = [];
+	promises.push(
+		new Promise((resolve, reject) => {
+			  	influx.query(`
+				    select * from measures
+				    where host = ${Influx.escape.stringLit(os.hostname())}
+				    order by time desc
+				    limit 1
+				  `).then(result => {
+				    //console.log('last db date ' + result[0].date);
+				    resolve(result[0]);
+				  }).catch(err => {
+				  	reject(err.stack);
+				})
+		})
+	)
+	promises.push(
+		new Promise((resolve, reject) => {
+				let json;
+				fs.readFile(sensorsDir, 'utf8', function(err, data) {  
+				    if (err){
+				    	console.log(err);
+				    	reject(err);
+				    } 
+				  	json = JSON.parse(data);
+				  	//console.log('last file date ' + json.date);
+				  	resolve(json);
+				});
+		})
+	)
+
+	Promise.all(promises)
+	.then(promises => {
+	  if(promises[0].date != promises[1].date){
+	  		console.log('new sensors value detected');
+	  		writeSensors(promises[1]);
+	  }
+	})
+}
+
+function compareLocation() {
+
+	const promises = [];
+
+	promises.push(
+		new Promise((resolve, reject) => {
+			  	influx.query(`
+				    select * from location
+				    where host = ${Influx.escape.stringLit(os.hostname())}
+				    order by time desc
+				    limit 1
+				  `).then(result => {
+				    //console.log('location')
+				    //console.log(result[0]);
+				    resolve(result[0]);
+
+				  }).catch(err => {
+				  	//console.log(err);
+				  	reject(err.stack);
+				})
+		})
+	)
+
+	promises.push(
+		new Promise((resolve, reject) => {
+			let json;
+			fs.readFile(gpsDir, 'utf8', function(err, data) {  
+			    if (err){
+			    	console.log(err);
+			    	reject(err);
+			    } 
+			  	resolve(data);
+			});
+		})
+	)
+
+
+	Promise.all(promises)
+	.then(promises => {
+		let result = promises[1];
+		let json = transformToJSON(result);
+
+		if(promises[0].date != json.date){
+			console.log('new location value detected');		
+			writeLocation(json);
+		}
+
+	})		
+
+}
+
+function compareRain() {
+
+	const promises = [];
+
+	promises.push(
+		new Promise((resolve, reject) => {
+			  	influx.query(`
+				    select * from rainfall
+				    where host = ${Influx.escape.stringLit(os.hostname())}
+				    order by time desc
+				    limit 1
+				  `).then(result => {
+				    resolve(result[0]);
+
+				  }).catch(err => {
+				  	reject(err.stack);
+				})
+		})
+	)
+
+	promises.push(
+		new Promise((resolve, reject) => {
+			let json;
+			fs.readFile(rainDir, 'utf8', function(err, data) {  
+			    if (err){
+			    	reject(err);
+			    }
+			    console.log(data);
+			  	resolve(data);
+			});
+		})
+	)
+
+
+	Promise.all(promises)
+	.then(promises => {
+
+		let result = promises[1];
+		let json   = transformLogToJSON(result);
+		console.log(json);
+		console.log(promises[0]);
+		if(promises[0].date != json.date){
+			console.log('new rain value detected');		
+			writeRain(json);
+		}
+
+	})		
+
+}
+
+function transformToJSON(string){
+		
+	let splitted  = string.split(',');
+	let date      = splitted[1];
+	let latitude  = splitted[2]/100;
+	let longitude = splitted[4]/100;
+
+
+	let json = 
+			{
+				"date"      : date,
+				"longitude" : longitude,
+				"latitude"  : latitude
+			}
+
+
+	return json;		
+}
+
+function transformLogToJSON(string) {
+
+	let json = {"date" : string};
+
+	return json;
+
+}
+
+function writeSensors(json) {
+	console.log("writing sensors...");
 	influx.writePoints([
 	  {
 	    measurement: 'measures',
@@ -79,12 +309,44 @@ function write(json) {
 	        wind_speed_avg: json.measure[5].value,
 		    wind_speed_max: json.measure[6].value,
 		    wind_speed_min: json.measure[7].value
-		},
+		}
 	  }
   ])
 }
 
+function writeLocation(json){
+	console.log("writing location...");
+	influx.writePoints([
+	  {
+	    measurement: 'location',
+	    tags: { host: os.hostname() },
+	    fields: {        
+		    date      : json.date,
+		    longitude : json.longitude,
+		    latitude  : json.latitude
+		}
+	  }
+  ])
+}
+
+function writeRain(json) {
+	console.log("writing rain...");
+
+	influx.writePoints([
+	  {
+	    measurement: 'rainfall',
+	    tags: { host: os.hostname() },
+	    fields: {        
+		    date: json.date
+		}
+	  }
+  ])
+}
 
 function loop() {
-	setTimeout(loop,100);
+	compareSensors();
+	compareLocation();
+	compareRain();
+	//console.log("test");
+	setTimeout(loop,1000);
 }
